f24c3b3f91108524d3d91698823f2d62a69d91ca
MunishAbhiram <munishabhiram@gmail.com>
2026-02-27 09:40:24 +0530

    UC10: Case-Insensitive & Space-Ignored Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,73 +2,20 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "level";
+        String input = "A man a plan a canal Panama";
 
-        PalindromeStrategy stackStrategy = new StackStrategy();
-        PalindromeStrategy dequeStrategy = new DequeStrategy();
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        long start1 = System.nanoTime();
-        boolean result1 = stackStrategy.check(input);
-        long end1 = System.nanoTime();
-        long time1 = end1 - start1;
+        boolean isPalindrome = true;
 
-        long start2 = System.nanoTime();
-        boolean result2 = dequeStrategy.check(input);
-        long end2 = System.nanoTime();
-        long time2 = end2 - start2;
+        for (int i = 0; i < normalized.length() / 2; i++) {
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
 
         System.out.println("Input : " + input);
-
-        System.out.println("\nStack Strategy:");
-        System.out.println("Is Palindrome? : " + result1);
-        System.out.println("Execution Time: " + time1 + " ns");
-
-        System.out.println("\nDeque Strategy:");
-        System.out.println("Is Palindrome? : " + result2);
-        System.out.println("Execution Time: " + time2 + " ns");
-    }
-}
-
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
-
-        java.util.Stack<Character> stack = new java.util.Stack<>();
-
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-
-class DequeStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
-
-        java.util.Deque<Character> deque = new java.util.ArrayDeque<>();
-
-        for (char c : input.toCharArray()) {
-            deque.addLast(c);
-        }
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                return false;
-            }
-        }
-
-        return true;
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
